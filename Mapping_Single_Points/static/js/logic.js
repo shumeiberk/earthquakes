9502ec7a5539f63919e4d15a3026167d51b46034
Ou <ous1@nala.roche.com>
2020-10-25 14:16:03 -0700

    updates

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -14,25 +14,18 @@ L.circle([34.0522, -118.2437], {
   radius: 300
 }).addTo(map);
 
-<<<<<<< HEAD
 // Get data from cities.js
 let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
 cityData.forEach(function(city) {
-	console.log(city)
-	L.circleMarker(city.location, {
+  console.log(city)
+  L.circleMarker(city.location, {
     radius: city.population/100000
   })
-	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
   .addTo(map);
 });
-=======
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
-
->>>>>>> 255c50d07036b5ec1b550fe0d2f3ce09ca2670c4
 
   // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -43,10 +36,3 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     
     // Then we add our 'graymap' tile layer to the map.
     streets.addTo(map);
-
-
-
-<<<<<<< HEAD
-
-=======
->>>>>>> 255c50d07036b5ec1b550fe0d2f3ce09ca2670c4
